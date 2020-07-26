986b66106b14523aa7ed6178910aaf4b8fbd6bb6
Tamas Fodor <fodortamas404@gmail.com>
2020-07-26 15:08:45 +0200

    Some basic navigation added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using IssueTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueTracker.Controllers
+{
+    public class AccountController : Controller
+    {
+        ////////////////////////////// LOGIN PAGE //////////////////////////////
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //TODO:post login
+
+
+
+        ////////////////////////////// USER DETAILS & EDIT //////////////////////////////
+
+        // GET: Account/Details
+        public ActionResult Details()
+        {
+            UserModel dummy = new UserModel("Dummy User", "justfor@its.sake", null, null, @"issa\path\man", "password");
+            return View(dummy);
+        }
+        // GET: Account/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+
+        ////////////////////////////// REGISTER //////////////////////////////
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        public ActionResult Register(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Controllers/IssueController.cs
@@ -0,0 +1,117 @@
+﻿using IssueTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueTracker.Controllers
+{
+    public class IssueController : Controller
+    {
+        // GET: Issue
+        public ActionResult Index()
+        {
+            //basic dummies
+            UserModel dummyOwner = new UserModel("dummy owner");
+            List<IssueModel> dummyIssues = new List<IssueModel> 
+            { 
+                new IssueModel("Main issue1", "Desription of the issue", dummyOwner),
+                new IssueModel("Main issue2", "Desription of second issue", dummyOwner),
+                new IssueModel("Main issue3", "You guessed right bro. Description of the 3rd issue", dummyOwner)
+            };
+
+            //deeper issues for another view
+            //List<UserModel> users = new List<UserModel>();
+            //UserModel user1 = new UserModel("User1");
+            //users.Add(user1);
+            //UserModel user2 = new UserModel("User2");
+            //users.Add(user2);
+            //UserModel user3 = new UserModel("User3");
+            //users.Add(user3);
+
+            //IssueModel issue1 = new IssueModel("issue1", "description of issue1", user1);
+            //dummyIssues.Add(issue1);
+            //IssueModel issue11 = new IssueModel("issue11", "description of issue11", issue1.AddedUsers.First(), issue1, IssueDifficulty.Easy);
+            //dummyIssues.Add(issue11);
+            //IssueModel issue12 = new IssueModel("issue12", "description of issue12", issue1.AddedUsers.First(), issue1, IssueDifficulty.Hard);
+            //dummyIssues.Add(issue12);
+            //issue11.Progress = 0.5;
+            //issue12.Progress = 0.7;
+
+            return View(dummyIssues);
+        }
+
+        // GET: Issue/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Issue/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Issue/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Issue/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Issue/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Issue/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Issue/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
